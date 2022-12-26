d433ee65385a1ab7f8d48a95713e307c5b2d57c7
C - 2 <C - 2@WIN-81H12M27IFP>
2022-12-26 21:57:24 +0500

    Добавьте файлы проекта.

---
+++ NetworkAccessibility/MainWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NetworkAccessibility
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<string> address = new List<string>();
+        PING _ping, _ping002;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _ping = new PING(FirstAddress.Text);
+            //_ping002 = new PING(SecondAddress.Text);
+        }
+        private List<string> AddAddress()
+        {
+            if (FirstAddress.Text != "")
+            {
+                address.Add(FirstAddress.Text);
+            }
+
+            if (SecondAddress.Text != "")
+            {
+                address.Add(SecondAddress.Text);
+            }
+
+            if (ThirdAddress.Text != "")
+            {
+                address.Add(ThirdAddress.Text);
+            }
+
+            if (FourthAddress.Text != "")
+            {
+                address.Add(FourthAddress.Text);
+            }
+
+            if (FifthAddress.Text != "")
+            {
+                address.Add(FifthAddress.Text);
+            }
+
+            if (SixthAddress.Text != "")
+            {
+                address.Add(SixthAddress.Text);
+            }
+            return address;
+        }
+
+        private void ShowRes_Click(object sender, RoutedEventArgs e)
+        {
+           
+
+            if (_ping.ShowAnswer(FirstAddress.Text))
+            {
+                
+                FirstRect.Fill = Brushes.Green;
+            }
+            else
+            {
+                
+                FirstRect.Fill = Brushes.Red;
+            }
+
+            //if (_ping.ShowAnswer(SecondAddress.Text))
+            //{
+            //    SecondRect.Fill = Brushes.Green;
+            //}
+            //else
+            //{
+            //    SecondRect.Fill = Brushes.Red;
+            //}
+
+            //if (_ping.ShowAnswer(AddAddress()))
+            //{
+            //    ThirdRect.Fill = Brushes.Green;
+            //}
+            //else
+            //{
+            //    ThirdRect.Fill = Brushes.Red;
+            //}
+
+            //if (_ping.ShowAnswer(AddAddress()))
+            //{
+            //    FourthRect.Fill = Brushes.Green;
+            //}
+            //else
+            //{
+            //    FourthRect.Fill = Brushes.Red;
+            //}
+
+            //if (_ping.ShowAnswer(AddAddress()))
+            //{
+            //    FifthRect.Fill = Brushes.Green;
+            //}
+            //else
+            //{
+            //    FifthRect.Fill = Brushes.Red;
+            //}
+
+            //if (_ping.ShowAnswer(AddAddress()))
+            //{
+            //    SixthRect.Fill = Brushes.Green;
+            //}
+            //else
+            //{
+            //    SixthRect.Fill = Brushes.Red;
+            //}
+        }
+    }
+}
+
---
+++ NetworkAccessibility/Ping.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace NetworkAccessibility
+{
+    internal class PING
+    {
+        private Ping myPing;
+        private PingOptions myPingOptions;
+        private string data;
+        private string myPath;
+        private string myLogFile;
+        private byte[] buffer;
+        private int myLength;
+        private DateTime CurrentTime;
+        private StreamWriter myStream;
+        private PingReply reply;
+        public PING(string args)
+        {
+            myPing = new Ping();
+            myPingOptions = new PingOptions();
+            data = "icmp_send_request";
+            myPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            myLogFile = @"\AccessLog.txt";
+            buffer = Encoding.ASCII.GetBytes(data);
+           // myLength = args.Count;
+            myStream = new StreamWriter(myPath + myLogFile);
+            CurrentTime = DateTime.Now;
+        }
+
+        public bool ShowAnswer(string args)
+        {
+            //for (int i = 0; i < myLength; i++)
+            //{
+                myPingOptions.DontFragment = false;
+                reply = myPing.Send(args, 120, buffer, myPingOptions);
+                if (reply.Status.ToString() == "Success")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            //}
+            //return true;
+        }
+    }
+}
